26fa54014e432f8bf20b6e48fd025513a14bbc1c
bbueno5000 <bbueno5000@gmail.com>
2018-05-22 14:15:11 -0600

    modified setup requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
 
 setuptools.setup(name='gym_vertical_landing',
-                 install_requires=['gym>=0.2.3', 'hfo_py>=0.2'],
+                 install_requires=['gym>=0.2.3'],
                  version='0.0.1')
